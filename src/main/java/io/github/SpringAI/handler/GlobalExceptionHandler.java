99eda136ac8cbadaef06e365f21b64e21b1cbb44
ZW L <liuzhengwei482@gmail.com>
2026-08-13 13:40:54 +0800

    feat:新增异常处理器

---
+++ src/main/java/io/github/SpringAI/handler/GlobalExceptionHandler.java
@@ -0,0 +1,28 @@
+package io.github.SpringAI.handler;
+
+import io.github.SpringAI.vo.ReturnVO;
+import org.springframework.web.bind.MethodArgumentNotValidException;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.RestControllerAdvice;
+
+/**
+ * @Description 全局异常处理器
+ * @Author 刘争伟
+ * @Date 2026/8/13 上午11:46
+ **/
+@RestControllerAdvice
+public class GlobalExceptionHandler {
+
+    @ExceptionHandler(MethodArgumentNotValidException.class)
+    public ReturnVO<Void> handleMethodArgumentNotVaildException(MethodArgumentNotValidException e){
+        String message = e.getBindingResult()
+                .getFieldError()
+                .getDefaultMessage();
+        return ReturnVO.fail(message);
+    }
+
+    @ExceptionHandler(Exception.class)
+    public ReturnVO<Void> handleException(Exception e){
+        return new  ReturnVO<>(500,"服务器异常",null);
+    }
+}
