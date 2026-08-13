2087795b5fb21d7206006374ec8fce112e0513e0
ZW L <liuzhengwei482@gmail.com>
2026-08-13 11:29:07 +0800

    feat:新增通用返回对象类

---
+++ src/main/java/io/github/SpringAI/vo/ReturnVO.java
@@ -0,0 +1,50 @@
+package io.github.SpringAI.vo;
+
+/**
+ * @Description 通用返回对象类
+ * @Author 刘争伟
+ * @Date 2026/8/13 上午9:24
+ **/
+public class ReturnVO<T> {
+
+    private Integer code;
+    private String message;
+    private T data;
+
+    public ReturnVO(){
+
+    }
+
+    public ReturnVO(Integer code,String message,T data){
+        this.code = code;
+        this.message = message;
+        this.data = data;
+    }
+
+    public static <T> ReturnVO<T> success(T data){
+        return new ReturnVO<>(200,"success",data);
+    }
+
+    public Integer getCode(){
+        return code;
+    }
+
+    public void setCode(Integer code){
+        this.code = code;
+    }
+
+    public String getMessage(){
+        return message;
+    }
+
+    public void setMessage(String message){
+        this.message = message;
+    }
+    public T getData(){
+        return data;
+    }
+
+    public void setData(T data){
+        this.data = data;
+    }
+}
