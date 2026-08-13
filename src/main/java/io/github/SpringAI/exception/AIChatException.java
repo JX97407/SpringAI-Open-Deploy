9639ab22801cbd549429406457fdd6d59849a613
ZW L <liuzhengwei482@gmail.com>
2026-08-13 17:38:42 +0800

    feat:新增AI对话异常类，区分全局异常

---
+++ src/main/java/io/github/SpringAI/exception/AIChatException.java
@@ -0,0 +1,17 @@
+package io.github.SpringAI.exception;
+
+/**
+ * @Description AI对话异常
+ * @Author 刘争伟
+ * @Date 2026/8/13 下午3:47
+ **/
+public class AIChatException extends RuntimeException{
+
+    public AIChatException(String message){
+        super(message);
+    }
+
+    public AIChatException(String message, Throwable cause){
+        super(message,cause);
+    }
+}
