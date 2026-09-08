4d70fff7623d8a3b13c457d4d5763696e9565abf
liuzhengwei <3271250872@qq.com>
2026-09-08 13:08:08 +0800

    feat:新增异常处理

--- src/main/java/io/github/SpringAI/handler/GlobalExceptionHandler.java
+++ src/main/java/io/github/SpringAI/handler/GlobalExceptionHandler.java
@@ -5,8 +5,10 @@ import io.github.SpringAI.exception.ChatSessionConflictException;
 import io.github.SpringAI.vo.ReturnVO;
 import org.springframework.http.converter.HttpMessageNotReadableException;
 import org.springframework.web.bind.MethodArgumentNotValidException;
+import org.springframework.web.bind.MissingServletRequestParameterException;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
+import org.springframework.web.method.annotation.MethodArgumentTypeMismatchException;
 
 /**
  * @Description 全局异常处理器
@@ -47,6 +49,22 @@ public class GlobalExceptionHandler {
     @ExceptionHandler(ChatSessionConflictException.class)
     public ReturnVO<Void> handleChatSessionConflictException(ChatSessionConflictException e){
         return ReturnVO.fail(409,e.getMessage());
+    }
+
+    /**
+     *处理请求中缺少的必填查询参数的异常
+     */
+    @ExceptionHandler(MissingServletRequestParameterException.class)
+    public ReturnVO<Void> handleMissingServletRequestParameterException(MissingServletRequestParameterException e){
+        return ReturnVO.fail(400,"缺少必要请求参数；" + e.getParameterName());
+    }
+
+    /**
+     *处理请求参数类型转换失败的异常
+     */
+    @ExceptionHandler(MethodArgumentTypeMismatchException.class)
+    public ReturnVO<Void> handleMethodArgumentTypeMismatchException(MethodArgumentTypeMismatchException e){
+        return ReturnVO.fail(400,"请求体参数错误" + e.getName());
     }
 
 }
