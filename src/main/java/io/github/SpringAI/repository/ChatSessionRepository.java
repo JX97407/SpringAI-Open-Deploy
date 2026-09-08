6ec5ee8dcbfdc1193c4e7b85e99f02918522b00e
liuzhengwei <3271250872@qq.com>
2026-09-08 12:57:52 +0800

    feat:新增注释

--- src/main/java/io/github/SpringAI/repository/ChatSessionRepository.java
+++ src/main/java/io/github/SpringAI/repository/ChatSessionRepository.java
@@ -8,6 +8,7 @@ package io.github.SpringAI.repository;
 import io.github.SpringAI.entity.ChatSession;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.List;
 import java.util.Optional;
 
 /**
@@ -15,7 +16,18 @@ import java.util.Optional;
  * @author lzw
  */
 public interface ChatSessionRepository  extends JpaRepository<ChatSession, Long> {
+    /**
+     *根据用户ID 和会话ID 查询指定会话
+     */
     Optional<ChatSession> findByUser_IdAndSessionId(Long userId, String sessionId);
 
+    /**
+     *根据会话ID 查询会话
+     */
     Optional<ChatSession> findBySessionId(String sessionId);
+
+    /**
+     * 查询指定用户的全部会话，并按更新时间倒序排列
+     */
+    List<ChatSession> findAllByUser_IdOrderByUpdatedAtDesc(Long userId);
 }
